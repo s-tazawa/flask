c7ba2acb2e70562797ddbc5b59f729f22c9d05ea
automagi-s-tazawa <shinichi.tazawa@automagi.jp>
2021-05-10 16:15:31 +0900

    0510

---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+# echo basename $0
+# mkdir -p /usr/src/app
+# RUN pip install -r requirements.txt
+# gunicorn startup:app -b :5000 --reload
+
+cd frontend
+npm start 
---
+++ scripts/stop_server.sh
@@ -0,0 +1,3 @@
+echo basename $0
+sudo systemctl stop nginx
+sudo systemctl stop uwsgi
